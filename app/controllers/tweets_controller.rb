6a3fa872b681ff3e39d8b188ed289706ff35067e
Daiki Uchiyama <sijimi777asari@gmail.com>
2020-02-26 23:38:11 +0900

    画像名変更

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,5 @@
 class TweetsController < ApplicationController
-  before_action :set_twitter_client, only: [:new, :create, :show]
+  before_action :set_twitter_client, only: [:create, :show]
 
   # BASE_IMAGE_PATH = './app/assets/images/test3.png'.freeze
   # BASE_IMAGE_PATH = @tweet.picture.path
@@ -15,7 +15,7 @@ class TweetsController < ApplicationController
 
   def new
     @tweet = Tweet.new
-    @twlink = @twitter.user.url.to_s
+    # @twlink = @twitter.user.url.to_s
   end
   
   def create
